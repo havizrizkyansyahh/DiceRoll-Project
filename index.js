27d6508dae6967d88ff0b04b72613f393615055d
Haviz Rizkyansyah <110213014+havizrizkyansyahh@users.noreply.github.com>
2023-09-06 08:55:04 +0700

    Add files via upload
    changed the logic

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-let playerOne = document.querySelectorAll(".playerOne .diceImg");
-let playerTwo = document.querySelectorAll(".playerTwo .diceImg");
-let randomizer1;
-let randomizer2;
-let banner = document.querySelector(".banner");
+function diceRoll () {
+    let randomizer1 = Math.floor(Math.random() * 6) + 1;
+    let randomizer2 = Math.floor(Math.random() * 6) + 1;
+    let diceImg1 = document.querySelector(".diceImg1");
+    let diceImg2 = document.querySelector(".diceImg2");
+    let banner = document.querySelector(".banner");
 
-function playerOneRoll () {
-    let diceImages = [...playerOne];
-    randomizer1 = Math.floor(Math.random() * 6);
-    let diceImage = diceImages[randomizer1];
-    diceImage.classList.add("active");
-}
+    diceImg1.setAttribute("src", "./images/Dice-" + randomizer1 + "-b.svg");
+    diceImg2.setAttribute("src", "./images/Dice-" + randomizer2 + "-b.svg");
 
-function playerTwoRoll () {
-    let diceImages = [...playerTwo];
-    randomizer2 = Math.floor(Math.random() * 6)
-    let diceImage = diceImages[randomizer2];
-    diceImage.classList.add("active");
-}
-
-function winDrawLose () {
     if (randomizer1 > randomizer2) {
         banner.innerHTML = "Player 1 Win";
     } else if (randomizer1 === randomizer2) {
         banner.innerHTML = "Draw";
-    } else if (randomizer1 < randomizer2) {
+    } else {
         banner.innerHTML = "Player 2 Win";
     }
-}
-
-function diceRoll () {
-    playerOneRoll();
-    playerTwoRoll();
-    winDrawLose();
 }
